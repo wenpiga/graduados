9482acafc27b9b1456b81d759b257ad4491774cd
wenpiga <wenpiga@hotmail.com>
2016-04-05 08:55:23 -0500

    actualizando

---
+++ grados/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from datetime import datetime, date, time, timedelta
+import calendar
+from  models import Alumno, Programa,userProfile
+from django.contrib.admin.views.decorators import staff_member_required
+#from import_export import resources
+
+from import_export.admin import ImportExportModelAdmin
+from import_export.admin import ImportExportActionModelAdmin
+
+from import_export import resources
+#from django.core.models import Alumno
+
+#from core.models import Alumno
+
+
+
+class AlumnoResource(resources.ModelResource):
+
+    class Meta:
+        model = Alumno
+      
+        #fields =('get_full_name','id','nit','nombres','programa','libro','acta','folio','fecha','diploma')    	     
+        fields =('get_full_name','id','nit','nombres','programa','libro','acta','folio','fecha.datetime.strptime(date_string, format)','diploma')
+
+
+class alumnoAdmin(ImportExportActionModelAdmin):
+
+	list_display=('nit','nombres','programa','libro','acta','folio','fecha','diploma')
+	#list_filter=('programa','nombres')
+	search_fields=['nombres','programa']
+
+	resource_class = AlumnoResource
+	
+	
+
+ 
+admin.site.register(Alumno,alumnoAdmin)
+admin.site.register(Programa)
+admin.site.register(userProfile)
+
---
+++ grados/forms.py
@@ -0,0 +1,82 @@
+from django import forms
+from models import Alumno
+from django.contrib.admin import widgets
+from models import Programa
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+
+
+
+#from django.forms import modelForm
+
+
+class ContactForm(forms.Form):
+	Email	=	forms.EmailField(widget=forms.TextInput())
+	Titulo	=	forms.CharField(widget=forms.TextInput())
+	Mensaje	=	forms.CharField(widget=forms.Textarea())
+
+
+
+class AddAlumnoForm(forms.Form):
+		nit			= forms.CharField(widget=forms.TextInput())
+		nombres		= forms.CharField(widget=forms.TextInput())
+		programa	= forms.ModelChoiceField(queryset=Programa.objects.all())
+		libro		= forms.CharField(widget=forms.TextInput())
+		acta		= forms.CharField(widget=forms.TextInput())
+		folio		= forms.CharField(widget=forms.TextInput())
+		'''fecha		=forms.DateField()'''
+		fecha		= forms.DateField(widget=widgets.AdminDateWidget()) 
+		diploma		= forms.CharField(widget=forms.TextInput())
+
+
+		'''class Meta:
+			db_table='alumno' '''
+
+		def clean(self):
+			return self.cleaned_data
+
+
+class LoginForm(forms.Form):
+	username	=	forms.CharField(widget=forms.TextInput())
+	password	=	forms.CharField(widget=forms.PasswordInput(render_value=False))
+		
+
+
+class AlumnoForm(forms.Form):
+	
+	class Meta:
+		model =	Alumno
+
+
+
+class ProgramaForm(ModelForm):
+	class Meta:
+		model = Programa 
+		fields = '__all__'
+
+class Miformulario(forms.Form):
+	usuario = forms.ModelChoiceField(queryset=User.objects.none())
+
+	def __init__(self,user,*args, ** kwargs):
+		super(Miformulario,self).__init__(*args, **kwargs)
+
+		if user.is_superuser:
+			self.fields['usuario'].queryset=User.objects.all()
+		else:
+			self.fields['usuario'].queryset = User.objects.filter(is_staff = True)
+			
+
+class ConsultaForm(forms.Form):
+
+	Fecha_Incial =	forms.DateField(widget=widgets.AdminDateWidget()) 
+	Fecha_Final = 	forms.DateField(widget=widgets.AdminDateWidget()) 
+	
+
+
+
+
+	
+
+
+
+
